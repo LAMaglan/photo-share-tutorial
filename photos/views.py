c0dbc3c41c9566df4932f53271d694a7e07d8241
Luigi Maglanoc <luigi.maglanoc@usit.uio.no>
2022-08-02 15:09:35 +0200

    user can now upload photos

--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from .models import Photo
 
 # Create your views here.
 def index(request):
+    if request.method == "POST":
+        new_photo = Photo(
+            file=request.FILES["img"]
+        )  # name as defined by form in index.html
+        new_photo.save()
     return render(request, "index.html")
 
